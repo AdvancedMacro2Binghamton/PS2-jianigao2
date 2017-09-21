97616f417669e63aea30c38c90de08b11b42aa1f
Jiani Gao <31349292+jennygjn@users.noreply.github.com>
2017-09-21 10:22:37 -0400

    Revert "trying to add another loop to calibriate A"
    This reverts commit 902b1bac9d749f138545e574c169558c6697396d.

--- VFIdeterministic.m
+++ VFIdeterministic.m
@@ -1,4 +1,5 @@
 close all
+<<<<<<< HEAD
 delta=0.025              %depreciation rate
 alpha=0.35               %capital share of income
 beta=0.99                %discount rate
@@ -44,6 +45,49 @@ while crit>epsi
     crit=max(abs(Tv-v))
     v=Tv
     i=i+1
+=======
+%%%% Set up parameters
+alpha = 0.35;
+beta = 0.99;
+delta = 0.025;
+sigma = 2;
+
+%%%% Set up discretized state space
+k_min = 0;
+k_max = 45;
+num_k = 1000; % number of points in the grid for k
+
+k = linspace(k_min, k_max, num_k);
+
+k_mat = repmat(k', [1 num_k]); % this will be useful in a bit
+
+%%%% Set up consumption and return function
+% 1st dim(rows): k today, 2nd dim (cols): k' chosen for tomorrow
+cons = k_mat .^ alpha + (1 - delta) * k_mat - k_mat'; 
+
+ret = cons .^ (1 - sigma) / (1 - sigma); % return function
+% negative consumption is not possible -> make it irrelevant by assigning
+% it very large negative utility
+ret(cons < 0) = -Inf;
+
+%%%% Iteration
+dis = 1; tol = 1e-06; % tolerance for stopping 
+v_guess = zeros(1, num_k);
+while dis > tol
+    % compute the utility value for all possible combinations of k and k':
+    value_mat = ret + beta * repmat(v_guess, [num_k 1]);
+    
+    % find the optimal k' for every k:
+    [vfn, pol_indx] = max(value_mat, [], 2);
+    vfn = vfn';
+    
+    % what is the distance between current guess and value function
+    dis = max(abs(vfn - v_guess));
+    
+    % if distance is larger than tolerance, update current guess and
+    % continue, otherwise exit the loop
+    v_guess = vfn;
+>>>>>>> parent of 902b1ba... trying to add another loop to calibriate A
 end
 
 plot(v(:,1))
